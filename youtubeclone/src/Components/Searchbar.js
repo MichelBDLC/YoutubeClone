b1122b9509e8147a0ce7ca609123752576b1f339
MichelBDLC <michelbdlc@gmail.com>
2023-03-12 13:27:50 -0700

    search base

--- youtubeclone/src/Components/Searchbar.js
+++ youtubeclone/src/Components/Searchbar.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 export default function Searchbar() {
 
-    //search fetch api here 
+    const [searchInput, setSearchInput] = useState("");
+
+    const apiKey = `AIzaSyB_js3m_6Td1BiAELR-giue7aIRmzHegEM`;
+
+    const apiSearchUrl = `https://youtube.googleapis.com/youtube/v3/search?q=` + searchInput + `&part=snippet&maxResults=10&key=` + apiKey;
+
+    //api data here.
 
     return (
         <>
-
+        <input type="text" />
+        <button type="submit"> Search </button>
         </>
     )
 }
