0b0436ce9b06b7810960d003dd024649d19e5f03
MaxWattan <mcwattanac@gmail.com>
2023-03-13 18:44:44 -0400

    first attempt

--- youtubeclone/src/Api/Fetch.js
+++ youtubeclone/src/Api/Fetch.js
@@ -1,7 +1,9 @@
-export default function fetch() {
+export function fetch(searchTerm) {
+   const KEY="AIzaSyDkl7o6yIOHtrFBGnoq4GXo9IzDBV6EPrw"
 
-    return (
-        <>
-        </>
-    )
+    return fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${searchTerm}&part=snippet&maxResults=10&key=${KEY}`)
+    .then((response) => response.json())
+    .then(response => console.log(response.json()))
 }
+
+
--- youtubeclone/src/App.js
+++ youtubeclone/src/App.js
@@ -1,12 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
+    <div className="wrapper">
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/videos/:id" element={<Video />} />
+
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
