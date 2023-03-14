388664e3ca67ce3df3d59e6da3e07db620cf6a5f
Angel <EMAIL>
2023-03-14 19:33:00 -0400

    search/logo/about

--- youtubeclone/src/App.js
+++ youtubeclone/src/App.js
@@ -1,19 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar'
-import About from './Components/About'
+import About from './Home/About'
 import './App.css';
+import Searchbar from './Components/Searchbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+        <Searchbar />
         <Routes>
           <Route path='/about' element={<About />}  />
         </Routes>
       </Router>
       <header className="App-header">
-       <p> TEST </p>
+       <p> </p>
       </header>
     </div>
   );
