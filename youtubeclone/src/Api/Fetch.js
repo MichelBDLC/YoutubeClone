ab13878c66fb9361c419029d13755f3701a88886
MaxWattan <mcwattanac@gmail.com>
2023-03-13 21:01:24 -0400

    testing 123

--- youtubeclone/src/Api/Fetch.js
+++ youtubeclone/src/Api/Fetch.js
@@ -1,20 +1,24 @@
-// export function fetch(searchTerm) {
-//    const KEY="AIzaSyDkl7o6yIOHtrFBGnoq4GXo9IzDBV6EPrw"
 
-//     return fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${searchTerm}&part=snippet&maxResults=10&key=${KEY}`)
-//     .then((response) => response.json())
-//     .then(response => console.log(response.json()))
-// }
-
-export default function fetch() {
-
-    const apiKey = `AIzaSyB_js3m_6Td1BiAELR-giue7aIRmzHegEM`;
-
-    const apiSearchUrl = `https://youtube.googleapis.com/youtube/v3/search?q=` + searchInput + `&part=snippet&maxResults=10&key=` + apiKey;
-
-    return (
-        <>
-        </>
-    )
+export function fetch(searchTerm) {
+    const KEY="AIzaSyDkl7o6yIOHtrFBGnoq4GXo9IzDBV6EPrw"
+    
+    
+    return fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${searchTerm}&part=snippet&maxResults=10&key=${KEY}`)
+    .then((response) => response.json())
+    .then(response => console.log(response.json()))
 }
+
+// import { useState } from "react";
+// export default function fetch() {
+
+//     const apiKey = `AIzaSyB_js3m_6Td1BiAELR-giue7aIRmzHegEM`;
+//     const [searchInput, setSearchInput] = useState("");
+
+//     const apiSearchUrl = `https://youtube.googleapis.com/youtube/v3/search?q=` + searchInput + `&part=snippet&maxResults=10&key=` + apiKey;
+
+//     return (
+//         <>
+//         </>
+//     )
+// };
 
