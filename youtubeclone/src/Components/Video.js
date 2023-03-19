99e84b5e37c79de346737060a7f52428786d0092
MichelBDLC <michelbdlc@gmail.com>
2023-03-19 14:48:01 -0700

    go back button

--- youtubeclone/src/Components/Video.js
+++ youtubeclone/src/Components/Video.js
@@ -1,10 +1,8 @@
-import { NavLink } from "react-router-dom";
-
-//is Card neccessary? TBD
+import { NavLink, useNavigate } from "react-router-dom";
 
 export default function Video(props) {
 
-    //const navigate = useNavigate();
+    const navigate = useNavigate();
 
     const dataTransfer = {
         theVideoId: props.videoId,
@@ -16,9 +14,12 @@ export default function Video(props) {
     };
 
     return (
+        <>
+        <button onClick={() => navigate(-1)}> Go Back </button>
         <div className="videos">
         <NavLink to="/VideoAction" state={dataTransfer}> <img src={props.videoThumbnail} alt={props.videoTitle} /> </NavLink>
         <NavLink to="/VideoAction" state={dataTransfer}> <strong> {props.videoTitle} </strong> </NavLink>
         </div>
+        </>
     )
 }
